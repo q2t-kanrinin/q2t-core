862d5adfbf83c9ddf6ba54db10aaa9b4aa631d0a
q2t-kanrinin <q2t.kanri@gmail.com>
2025-07-08 10:44:20 +0900

    Add Graphviz generator with semantic tooltips

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
 from .dsl_parser import DSLParser
+from .graphviz_generator import generate_graphviz_from_fold_dsl
 from src.validators.check_structure import validate_links
 
-__all__ = ["DSLParser", "validate_links"]
+__all__ = ["DSLParser", "generate_graphviz_from_fold_dsl", "validate_links"]
